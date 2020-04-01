02bc508e1b4bd7d6bf2cf0de416ba9901dfc1b1f
scy <348691356@qq.com>
2020-04-01 20:29:24 +0800

    反射调用

--- tensquare_article/src/main/java/com/tensquare/article/service/ReflectServinceImpl.java
+++ tensquare_article/src/main/java/com/tensquare/article/service/ReflectServinceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.web.context.support.WebApplicationObjectSupport;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: scyang
@@ -62,10 +64,47 @@ public class ReflectServinceImpl extends WebApplicationObjectSupport implements 
     @Override
     public Object reflectMethodByAnyParams(String beanName, String methodName, Object... paramObject)
             throws InvocationTargetException,
-            ClaimpptException,
-            IllegalAccessException,
+            ClaimpptException, IllegalAccessException,
             NoSuchMethodException {
+        /**
+         * @Description:  反射调用
+         * @methodName: reflectMethodByAnyParams
+         * @Param: [beanName, methodName, paramObject]
+         * @return: java.lang.Object
+         * @Author: scyang
+         * @Date: 2020/4/1 20:07
+         */
 
-        return this.reflectMethodByOneParam(beanName, methodName, paramObject);
+        /** 通过beanName获取对象 */
+        Object object = this.getBeanByName(beanName);
+        /** 获取字节码对象 */
+        Class<?> clazz = object.getClass();
+        Method method=null;
+        Object tagert=null;
+        /** 字节码集合 */
+        List<Class> classList=new ArrayList<>();
+        /** 参数列表集合 */
+        List<Object> paramList=new ArrayList<>();
+        try {
+            if (paramObject!=null && paramObject.length>0){
+                for (Object param : paramObject) {
+                    /** 添加参数类型字节码 */
+                    classList.add(param.getClass());
+                    /** 添加方法参数列表 */
+                    paramList.add(param);
+                }
+                /** 反射调用 */
+                method = clazz.getDeclaredMethod(methodName, classList.toArray(new Class[classList.size()]));
+                tagert= method.invoke(object,paramList.toArray(new Object[paramList.size()]));
+            }
+            else {
+                method = clazz.getDeclaredMethod(methodName);
+                tagert= method.invoke(object);
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new ClaimpptException("反射调用失败 "+e.getMessage());
+        }
+        return tagert;
     }
 }
