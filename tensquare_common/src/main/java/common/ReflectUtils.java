5c557f5fb51808ff9dae30973c4bd0bc9674c4b3
scy <348691356@qq.com>
2020-04-02 00:03:06 +0800

    反射调用工具类

---
+++ tensquare_common/src/main/java/common/ReflectUtils.java
@@ -0,0 +1,41 @@
+package common;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: scyang
+ * @program: tensquare_parent
+ * @package: common
+ * @date: 2020-04-01 23:38:18
+ * @describe: 反射调用工具类
+ */
+public class ReflectUtils {
+    private static final ReflectUtils instance_=new ReflectUtils();
+
+    public static ReflectUtils getInstance(){
+        return instance_;
+    }
+
+    public <T,V> V reflectMethodByParams(T t,String methodName,Object...params) throws Exception {
+        V v=null;
+        Class<T> clazz = (Class<T>) t.getClass();
+        T instance = clazz.newInstance();
+        if (params!=null&&params.length>0){
+            List<Class> calssList=new ArrayList<>();
+            List<Object> paramList=new ArrayList<>();
+            for (Object param : params) {
+                calssList.add(param.getClass());
+                paramList.add(param);
+            }
+            v= (V) clazz.getDeclaredMethod(methodName, calssList.toArray(new Class[calssList.size()]))
+                    .invoke(instance,paramList.toArray(new Object[paramList.size()]));
+        }
+        else {
+          v= (V) clazz.getDeclaredMethod(methodName,new Class[]{})
+                    .invoke(instance,new Object[]{});
+        }
+        return v;
+    }
+}
