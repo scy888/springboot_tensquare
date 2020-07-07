f217b606d36f7f9e70058e689e3b190a121428a5
scy <348691356@qq.com>
2020-07-08 00:45:59 +0800

    测试一分钱优化子查询

--- tensquare_test/src/test/java/com/tensquare/test/DomeTest.java
+++ tensquare_test/src/test/java/com/tensquare/test/DomeTest.java
@@ -1,11 +1,17 @@
 package com.tensquare.test;
 
+import com.tensquare.test.dao.UserDomeDao;
+import com.tensquare.test.pojo.User;
+import common.JacksonUtils;
 import common.NumUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 /**
  * @author: scyang
@@ -14,13 +20,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @date: 2020-07-04 12:36:25
  * @describe:
  */
+@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = TestApplication.class)
+@SpringBootTest
 public class DomeTest {
     @Autowired
-    private NumUtils numUtils;
+    private UserDomeDao userDomeDao;
+    @Autowired
+    private JacksonUtils jacksonUtils;
+
     @Test
     public void test01(){
-        System.out.println(66);
+        List<User> userList = userDomeDao.getList("男");
+        log.info("userList:{}",jacksonUtils.toString(userList));
     }
 }
